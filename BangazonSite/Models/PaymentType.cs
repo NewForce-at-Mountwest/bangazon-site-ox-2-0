426d8e9f23bb4311c9ce7f77c7d97cce001f5162
Ryan Clay <clay.Christopher29@yahoo.com>
2020-01-21 13:06:37 -0500

    profile added

--- BangazonSite/Models/PaymentType.cs
+++ BangazonSite/Models/PaymentType.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,12 +8,16 @@ namespace BangazonSite.Models
 {
     public class PaymentType
     {
+        
         public int PaymentTypeId { get; set; }
+        
         public string Name { get; set; }
+        [Display(Name= "Account Number")]
         public string acctNumber { get; set; }
+        [Display(Name=" In Use")]
         public bool isActive { get; set; }
-
         public string UserId { get; set; }
+        
         public ApplicationUser User { get; set; }
         public List<Order> Orders { get; set; } = new List<Order>();
     }
