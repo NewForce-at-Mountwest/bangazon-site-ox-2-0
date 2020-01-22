2dead9e25fa3f9c15ffb2637ee9efb53837b41e8
lmsamples <lmarcuss2000@gmail.com>
2020-01-22 11:56:22 -0500

    ready to pr home page

--- BangazonSite/Controllers/HomeController.cs
+++ BangazonSite/Controllers/HomeController.cs
@@ -1,29 +1,55 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using BangazonSite.Data;
 using BangazonSite.Models;
+using Microsoft.AspNetCore.Identity;
+using System.Diagnostics;
 
 namespace BangazonSite.Controllers
 {
     public class HomeController : Controller
-    {
-        private readonly ILogger<HomeController> _logger;
+	{
+		private readonly ApplicationDbContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
+		public HomeController(ApplicationDbContext context)
+		{
+			_context = context;
+		}
 
-        public IActionResult Index()
-        {
-            return View();
-        }
+		// GET: Products
+		public async Task<IActionResult> Index()
+		{
+			var applicationDbContext = await _context.Product.Include(p => p.ProductType).Include(p => p.User)
+				.OrderByDescending(p => p.Id).Take(20).ToListAsync();
+			var currentProducts = applicationDbContext.Where(p => !p.isArchived);
+			return View(currentProducts);
+		}
 
-        public IActionResult Privacy()
+		public async Task<IActionResult> Details(int? id)
+		{
+			if (id == null)
+			{
+				return NotFound();
+			}
+
+			var product = await _context.Product
+				.Include(p => p.ProductType)
+				.Include(p => p.User)
+				.FirstOrDefaultAsync(m => m.Id == id);
+			if (product == null)
+			{
+				return NotFound();
+			}
+
+			return View(product);
+		}
+
+		public IActionResult Privacy()
         {
             return View();
         }
